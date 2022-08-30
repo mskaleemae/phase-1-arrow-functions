528ba743d6157ce77decd5f43cacb38febb2161c
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-30 15:11:20 -0400

    commit tree 29c4bd3b45891af1af6bf0b0fa45dcc8c2a782d5

---
+++ index.js
@@ -0,0 +1,13 @@
+// let divide = (2000, 100) => 2000/100;
+
+// // const square = 2 => 2 * 2
+// // square(2);
+
+// const add = (a,b) => {
+//     return 3 + 4;
+// }
+// add(3, 4);
+
+let divide = (a, b) => a / b;
+let square = x => x * x;
+let add = (a, b) => a + b;
